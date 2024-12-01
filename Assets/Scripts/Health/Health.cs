1063ed0e9710cf5527271a6a4359ce3335c191a2
caio10012 <116567019+caio10012@users.noreply.github.com>
2024-11-30 23:02:02 -0300

    Continuação
    IFRAMES + Invencibilidade depois de tomar dano

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,16 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
+
+    [Header("iframes")]
+    [SerializeField] private float iframesDuration;
+    [SerializeField] private int numberOfflashes;
+    private SpriteRenderer spriteRend;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,6 +36,7 @@ public class Health : MonoBehaviour
             // player se machucou
             anim.SetTrigger("hurt");
             //iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -45,4 +55,18 @@ public class Health : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfflashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iframesDuration / (numberOfflashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iframesDuration / (numberOfflashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
+
 }
