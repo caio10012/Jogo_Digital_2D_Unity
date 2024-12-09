12172e902e26717f37960c85048555f600fd78d1
caio10012 <116567019+caio10012@users.noreply.github.com>
2024-12-08 23:48:59 -0300

    Main menu

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Start Menu")]
+    [SerializeField] private GameObject startMenu;
+
     [Header("Game Over")]
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private AudioClip gameOverSound;
@@ -15,21 +18,22 @@ public class UIManager : MonoBehaviour
     {
         gameOverScreen.SetActive(false);
         pauseScreen.SetActive(false);
+        Time.timeScale = 1; // Certifique-se de que a escala de tempo está correta ao iniciar o jogo
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
-           if(pauseScreen.activeInHierarchy)
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseScreen.activeInHierarchy)
                 PauseGame(false);
             else
                 PauseGame(true);
+        }
     }
+
     #region Game Over
-
-
-
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
@@ -38,11 +42,13 @@ public class UIManager : MonoBehaviour
 
     public void Restart()
     {
+        Time.timeScale = 1; // Redefinir a escala de tempo ao reiniciar o jogo
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void Menu()
     {
+        Time.timeScale = 1; // Redefinir a escala de tempo ao voltar para o menu
         SceneManager.LoadScene(0);
     }
 
@@ -50,9 +56,9 @@ public class UIManager : MonoBehaviour
     {
         Application.Quit();
 
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+#endif
     }
     #endregion
 
@@ -61,21 +67,32 @@ public class UIManager : MonoBehaviour
     {
         pauseScreen.SetActive(status);
 
-        if(status)
+        if (status)
             Time.timeScale = 0;
         else
             Time.timeScale = 1;
-       
-          
     }
 
     public void SoundVolume()
     {
         SoundManager.instance.ChangeSoundVolume(0.2f);
     }
+
     public void MusicVolume()
     {
         SoundManager.instance.ChangeMusicVolume(0.2f);
     }
     #endregion
+
+    public void Play()
+    {
+        Time.timeScale = 1; // Redefinir a escala de tempo ao iniciar o jogo
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitStart()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
 }
