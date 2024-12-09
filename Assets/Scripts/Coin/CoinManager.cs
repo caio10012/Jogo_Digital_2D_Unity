9d981b5ba1d9173bee5157762539ef31ac5359a6
caio10012 <116567019+caio10012@users.noreply.github.com>
2024-12-09 18:54:17 -0300

    Correção level2 + save
    Moedas são salvas quando o player muda de cenário(level) e são resetadas quando o player sai do modo de jogo.

--- Assets/Scripts/Coin/CoinManager.cs
+++ Assets/Scripts/Coin/CoinManager.cs
@@ -3,7 +3,6 @@ using UnityEngine.UI;
 
 public class CoinManager : MonoBehaviour
 {
-
     public int coinCount;
     public Text coinText;
     public GameObject bridge;
@@ -12,18 +11,32 @@ public class CoinManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Carrega o número de moedas salvo
+        coinCount = PlayerPrefs.GetInt("CoinCount", 0);
+        UpdateCoinText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        coinText.text = ": " + coinCount.ToString();
+        UpdateCoinText();
         if (coinCount == 10 && !bridgeDestroyed)
         {
             bridgeDestroyed = true;
             Destroy(bridge);
         }
     }
-    
+
+    // Atualiza o texto das moedas
+    void UpdateCoinText()
+    {
+        coinText.text = ": " + coinCount.ToString();
+    }
+
+    // Método chamado quando o objeto é destruído
+    void OnApplicationQuit()
+    {
+        // Reseta o contador de moedas quando o jogo é reiniciado
+        PlayerPrefs.DeleteKey("CoinCount");
+    }
 }
--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-
     [SerializeField] private Transform previousRoom;
     [SerializeField] private Transform nextRoom;
     [SerializeField] private CameraController cam;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void Awake()
@@ -27,18 +28,30 @@ public class Door : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
+            // Encontra o CoinManager na cena
+            CoinManager coinManager = FindObjectOfType<CoinManager>();
+            if (coinManager != null)
+            {
+                // Salva o número de moedas antes de mudar de cena
+                PlayerPrefs.SetInt("CoinCount", coinManager.coinCount);
+                PlayerPrefs.Save();
+            }
+
+            // Carrega a próxima cena no índice
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
             if (collision.transform.position.x < transform.position.x)
             {
                 cam.MoveToNewRoom(nextRoom);
                 nextRoom.GetComponent<Room>().ActivateRoom(true);
                 previousRoom.GetComponent<Room>().ActivateRoom(false);
-            }        
+            }
             else
             {
                 cam.MoveToNewRoom(previousRoom);
                 previousRoom.GetComponent<Room>().ActivateRoom(true);
                 nextRoom.GetComponent<Room>().ActivateRoom(false);
-            }     
+            }
         }
     }
 }
